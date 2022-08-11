df9fabf0ae325d356eb1f1a807fa5daf05026341
Dahdo <dndashimye15@gmail.com>
2022-08-11 23:07:25 +0200

    showing location coordinates

--- app/src/main/java/com/dahdotech/testlocation/MainActivity.java
+++ app/src/main/java/com/dahdotech/testlocation/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dahdotech.testlocation;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
@@ -13,8 +14,10 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
@@ -38,6 +41,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
@@ -53,6 +57,7 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
     private ArrayList<String> permissionsRejected = new ArrayList<>();
     private TextView locationTextView;
     private LocationRequest locationRequest;
+    private static final int ALL_PERMISSIONS_RESULT = 111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +73,14 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
         permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
 
         permissionToRequest = permissionToRequest(permissions);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(permissionToRequest.size() > 0){
+                requestPermissions(permissionToRequest.toArray(
+                        new String[permissionToRequest.size()]),
+                        ALL_PERMISSIONS_RESULT
+                );
+            }
+        }
         client = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .addOnConnectionFailedListener(this)
@@ -149,7 +162,9 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-
+        if(location != null){
+            locationTextView.setText(String.format("Lat: %s Lon: %s", location.getLatitude(), location.getLongitude()));
+        }
     }
 
     @Override
@@ -188,6 +203,59 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
             Toast.makeText(MainActivity.this, "You need to enable location",
                     Toast.LENGTH_LONG).show();
         }
+        LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                .requestLocationUpdates(locationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationAvailability(@NonNull LocationAvailability locationAvailability) {
+                        super.onLocationAvailability(locationAvailability);
+                    }
+
+                    @Override
+                    public void onLocationResult(@NonNull LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        if(locationResult != null){
+                            Location location = locationResult.getLastLocation();
+                            locationTextView.setText(MessageFormat.format("Lat: {0} Lon: {1}", location.getLatitude(), location.getLongitude()));
+                        }
+                    }
+                }, null);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case ALL_PERMISSIONS_RESULT:
+                for(String perm : permissionToRequest){
+                    if(!hasPermission(perm)){
+                        permissionsRejected.add(perm);
+                    }
+                }
+                if(permissionsRejected.size() > 0){
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                        if(shouldShowRequestPermissionRationale(permissionsRejected.get(0))){
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            builder.setMessage("These permissions are mandoatory")
+                                    .setPositiveButton("OK", (dialogInterface, which) -> {
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                                            requestPermissions(permissionsRejected.toArray(
+                                                    new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT
+                                            );
+                                }
+
+                            }).setNegativeButton("Cancel", null).create().show();
+                        }
+                    }
+                }
+                else{
+                    if(client != null){
+                        client.connect();
+                    }
+                }
+                break;
+        }
+
+
     }
 
     @Override
